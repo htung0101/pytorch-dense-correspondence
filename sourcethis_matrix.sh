5a6eb07e4fec63d0d24a25712c0094255fb310e3
fishtung <htung@andrew.cmu.edu>
2020-12-22 09:26:43 -0500

    sftp and source on matrxi

---
+++ sourcethis_matrix.sh
@@ -0,0 +1,5 @@
+export DC_SOURCE_DIR=/home/htung/2020/pytorch-dense-correspondence
+export PYTHONPATH=/home/htung/2020/pytorch-dense-correspondence/modules:$PYTHONPATH
+export DC_DATA_DIR=/home/htung/2020/pdc
+
+source activate py36-baselines
